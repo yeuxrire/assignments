964bbf1976a4802cfce06f7d31168cf9ed1cd162
yeuxrire <yeuxrire@sookmyung.ac.kr>
2026-04-09 14:57:42 +0900

    lsp lab04 source code updated

--- lsp/lab04/list2.c
+++ lsp/lab04/list2.c
@@ -10,46 +10,105 @@
 
 char type(mode_t);
 char *perm(mode_t);
-void printStat(char*, char*, struct stat*);
+void printStat(char*, char*, struct stat*, int, int, int, int);
 
 /* 디렉터리 내용을 자세히 리스트한다. */
 int main(int argc, char **argv)
 {
     DIR *dp;
-    char *dir;
+    char *dir = ".";
     struct stat st;
     struct dirent *d;
     char path[BUFSIZ+1];
 
-    if (argc == 1)
-        dir = ".";
-    else dir = argv[1];
-
-    if ((dp = opendir(dir)) == NULL) // 디렉터리 열기
-        perror(dir);
+    int opt_a = 0, opt_l = 0, opt_i = 0, opt_s = 0, opt_r = 0;
     
-    while ((d = readdir(dp)) != NULL) { // 디렉터리의 각 파일에 대해
-        sprintf(path, "%s/%s", dir, d->d_name); // 파일경로명 만들기
-        if (lstat(path, &st) < 0)       // 파일 상태 정보 가져오기
-            perror(path);
-        else
-            printStat(path, d->d_name, &st); // 상태 정보 출력
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            for (int j = 1; argv[i][j] != '\0'; j++) {
+                switch (argv[i][j]){
+                    case 'a': opt_a = 1; break;
+                    case 'l': opt_l = 1; break;
+                    case 'i': opt_i = 1; break;
+                    case 's': opt_s = 1; break;
+                    case 'r': opt_r = 1; break;
+                    default:
+                        fprintf(stderr, "Usage: %s[-alisr] [directory]\n", argv[0]);
+                        exit(1);
+                }
+            }
+        } else {
+            dir = argv[i];
+        }
     }
 
+    if ((dp = opendir(dir)) == NULL) {// 디렉터리 열기
+        perror(dir);
+        exit(1);
+    }
+
+    char *file_list[1024];
+    int file_count = 0;
+    
+    while ((d = readdir(dp)) != NULL) { 
+        if (opt_a == 0 && d->d_name[0] == '.') {
+            continue;
+        }
+        if (file_count < 1024) {
+            file_list[file_count] =strdup(d->d_name);
+            file_count++;
+        } else {
+            fprintf(stderr, "경고: 파일이 1024개를 초과하여 일부만 읽습니다.\n");
+            break;
+        }
+    }
     closedir(dp);
+
+    if (opt_r) {
+        for (int i = file_count -1; i >= 0; i--) {
+            sprintf(path, "%s/%s", dir, file_list[i]);
+            if (lstat(path, &st) < 0)
+                perror(path);
+            else
+                printStat(path, file_list[i], &st, opt_a, opt_l, opt_i, opt_s);
+            
+            free(file_list[i]);
+        }
+    } else {
+        for (int i = 0; i < file_count; i++) {
+            sprintf(path, "%s/%s", dir, file_list[i]);
+            if (lstat(path, &st) < 0)
+                perror(path);
+            else
+                printStat(path, file_list[i], &st, opt_a, opt_l, opt_i, opt_s);
+            
+            free(file_list[i]);
+        }
+    }
     exit(0);
 }
 
 /* 파일 상태 정보를 출력 */
-void printStat(char *pathname, char *file, struct stat *st)
+void printStat(char *pathname, char *file, struct stat *st, int opt_a, int opt_l, int opt_i, int opt_s)
 {
-    printf("%5d ", st->st_blocks);
+    if (opt_i) {
+        printf("%llu ", (unsigned long long)st->st_ino);
+    }
+
+    if (opt_s) {
+        printf("%4ld ", (long)(st->st_blocks / 2));
+    }
+
+    if (opt_l) {
     printf("%c%s ", type(st->st_mode), perm(st->st_mode));
-    printf("%3d ", st->st_nlink);
+    printf("%3lu ", (unsigned long)st->st_nlink);
     printf("%s %s ", getpwuid(st->st_uid)->pw_name, getgrgid(st->st_gid)->gr_name);
-    printf("%9d ", st-> st_size);
+    printf("%9lld ", (long long)st-> st_size);
     printf("%.12s ", ctime(&st->st_mtime)+4);
+    }
+
     printf("%s\n", file);
+    
 }
 
 /* 파일 타입을 반환 */
